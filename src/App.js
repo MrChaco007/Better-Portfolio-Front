07e65a2bf16c30e8876601dfe94fd1b14b884ba6
Alexander Chaconas <alexander.chaconas@gmail.com>
2021-03-10 13:58:54 -0500

    design update

--- src/App.js
+++ src/App.js
@@ -188,23 +188,23 @@ function App() {
 
   return (
     <div id="app">
-        <Navbar id="Nav" collapseOnSelect expand="md">
-          <Navbar.Brand>
-            <img
-              id="brand"
-              src="https://res.cloudinary.com/dvnl2s9um/image/upload/v1609191931/Personal_Logo_1_adiipp.png"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#intro">INTRO</Nav.Link>
-              <Nav.Link href="#projects">PROJECTS</Nav.Link>
-              <Nav.Link href="#who">WHO</Nav.Link>
-              <Nav.Link href="#contact">CONTACT</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <Navbar id="Nav" collapseOnSelect expand="md">
+        <Navbar.Brand>
+          <img
+            id="brand"
+            src="https://res.cloudinary.com/dvnl2s9um/image/upload/v1609191931/Personal_Logo_1_adiipp.png"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#intro">INTRO</Nav.Link>
+            <Nav.Link href="#projects">PROJECTS</Nav.Link>
+            <Nav.Link href="#who">WHO</Nav.Link>
+            <Nav.Link href="#contact">CONTACT</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <section id="intro">
         <div className="background">
           <span></span>
@@ -228,19 +228,21 @@ function App() {
           <span></span>
           <span></span>
         </div>
-        <h2 id="intro-header">
-          Hello, I'm <span id="name2">Alexander Chaconas</span>. <br />
-          I'm a full-stack developer.
-        </h2>
-        <p id="scroll-header">View my work</p>
-        <div className="mouse_scroll">
-          <div className="mouse">
-            <div className="wheel"></div>
-          </div>
-          <div>
-            <span className="m_scroll_arrows unu"></span>
-            <span className="m_scroll_arrows doi"></span>
-            <span className="m_scroll_arrows trei"></span>
+        <div id="header">
+          <h2 id="header-text">
+            Hello, I'm <span id="name2">Alexander Chaconas</span>. <br />
+            I'm a full-stack developer.
+          </h2>
+          <p id="scroll-header">View my work</p>
+          <div className="mouse_scroll">
+            <div className="mouse">
+              <div className="wheel"></div>
+            </div>
+            <div>
+              <span className="m_scroll_arrows unu"></span>
+              <span className="m_scroll_arrows doi"></span>
+              <span className="m_scroll_arrows trei"></span>
+            </div>
           </div>
         </div>
       </section>
